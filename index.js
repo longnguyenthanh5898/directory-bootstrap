1ef8cf1e8b230b994463654e2683dcb29c480766
unknown <louis.nguyen8022@gmail.com>
2022-09-13 15:13:46 +0700

    update slide navbar

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-var navbarInput = document.getElementById("search_search")
- inputLength = navbarInput.offsetWidth
- var onFocus = false;
+var backgroundImage = document.getElementById("slide")
 
-if( onFocus === false && navbarInput.onfocus ){
-    navbarInput.style.width = inputLength + "32px" 
-    onFocus = true;
-} else{
-    navbarInput.style.width = inputLength
-}
+
+const backgroundURL = [
+    "https://d19m59y37dris4.cloudfront.net/directory/2-0-1/img/photo/photo-1501621965065-c6e1cf6b53e2.jpg",
+    "https://d19m59y37dris4.cloudfront.net/directory/2-0-1/img/photo/photo-1519974719765-e6559eac2575.jpg",
+    "https://d19m59y37dris4.cloudfront.net/directory/2-0-1/img/photo/photo-1519974719765-e6559eac2575.jpg",
+    "https://d19m59y37dris4.cloudfront.net/directory/2-0-1/img/photo/photo-1534850336045-c6c6d287f89e.jpg"
+]
+var counter = 0; 
+var maxImage = 4; 
+setInterval(function() {
+    backgroundImage.style.backgroundImage = `url(${backgroundURL[counter]})`
+    if (counter + 1 == maxImage) {
+        counter = 0; //reset to start
+    } else {
+        ++counter; //iterate to next image
+    }
+}, 5000);
